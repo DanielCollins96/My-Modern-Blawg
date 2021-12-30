ecb4de4c2956a01f893d77815923dbb057e1e122
Dan <danielmcollins1996@gmail.com>
2021-12-30 00:18:00 -0700

    removing premade posts and site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Technology and Programming Blog',
+  author: 'Dan Collins',
+  headerTitle: 'TechDive',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/DanielCollins96/My-Modern-Blawg.git',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
