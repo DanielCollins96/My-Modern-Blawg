16f69864eaf0cc3f2bc848f52003ed637501d9d6
Dan <danielmcollins1996@gmail.com>
2022-01-18 16:37:10 -0700

    homepage aside changes

--- components/AsideBio.js
+++ components/AsideBio.js
@@ -3,7 +3,7 @@ import SocialIcon from '@/components/social-icons'
 
 const AsideBio = () => {
   return (
-    <aside className="flex flex-col gap-4 p-2  mb-auto">
+    <aside className="flex flex-col gap-4 py-2 px-4  mb-3 border-t-4 border-l-2  border-slate-400 border">
       <p className="font-bold text-4xl mb-2 mt-2">About This Site</p>
       <p className="text-lg leading-6 text-gray-600 dark:text-gray-500">
         I'm a Canadian Software Developer trying out blogging. I'll be making some content about
@@ -11,7 +11,7 @@ const AsideBio = () => {
         <br></br>
         Feel free to reach out to me with questions or posts you'd like to see.
       </p>
-      <div className="grid grid-flow-col justify-center gap-4">
+      <div className="grid grid-flow-col justify-center gap-4 mb-2">
         <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
         <SocialIcon kind="github" href={siteMetadata.github} size="6" />
       </div>
