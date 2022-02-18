5610c49ec53c503874edb671d0b5329dab3e9776
Dan <danielmcollins1996@gmail.com>
2022-02-18 13:46:58 -0700

    Aside drafting

--- components/AsideBio.js
+++ components/AsideBio.js
@@ -4,11 +4,12 @@ import SocialIcon from '@/components/social-icons'
 const AsideBio = () => {
   return (
     <aside className="flex flex-col gap-4 py-2 px-4  mb-3 border-t-4 border-l-2  border-slate-400 border">
-      <p className="font-bold text-4xl mb-2 mt-2">About This Site</p>
-      <p className="text-lg leading-6 text-gray-600 dark:text-gray-500">
-        I'm a Canadian Software Developer trying out blogging. I'll be making some content about
-        software and technology, content that is hopefully useful to readers <br></br>
-        <br></br>
+      <p className="font-bold text-4xl mb-2 mt-2">Learning + Writing</p>
+      <p className="text-lg leading-6 text-gray-700 dark:text-gray-200">
+        I'm a Canadian Software Developer trying out blogging. I'll be writing about software,
+        technology but maybe add a flair of literature, economics, sports or photography
+      </p>
+      <p className="text-lg leading-6 text-gray-700 dark:text-gray-200">
         Feel free to reach out to me with questions or posts you'd like to see.
       </p>
       <div className="grid grid-flow-col justify-center gap-4 mb-2">
