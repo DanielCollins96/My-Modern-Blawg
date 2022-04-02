e1e915f10dfb94105a6df09367ef65d90d1304fa
Dan <danielmcollins1996@gmail.com>
2022-04-02 14:09:05 -0600

    adding image shadow

--- components/AsideBio.js
+++ components/AsideBio.js
@@ -4,7 +4,11 @@ import SocialIcon from '@/components/social-icons'
 const AsideBio = () => {
   return (
     <aside className="flex flex-col gap-4 py-2 px-3  mb-3  border-slate-400 ">
-      <img src="/static/images/headshot.jpeg" alt="" className="rounded-full w-56 m-auto" />
+      <img
+        src="/static/images/headshot.jpeg"
+        alt=""
+        className="rounded-full w-56 m-auto shadow-2xl"
+      />
       <p className="font-bold text-4xl mb-2 mt-2">Learning + Writing</p>
       <p className="text-md leading-6 text-gray-700 dark:text-gray-200">
         I'm a Canadian Software Developer trying out blogging. I'll be writing about software and
