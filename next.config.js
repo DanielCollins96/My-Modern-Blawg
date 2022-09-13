828f4df042050a9e6fc0d815d8defb7d6583e31f
Dan <danielmcollins1996@gmail.com>
2022-09-13 15:50:05 -0600

    CSP for analytics

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
+  default-src 'self' www.google-analytics.com 'unsafe-inline';
   script-src 'self' 'unsafe-eval' 'unsafe-inline';
   style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self' fonts.gstatic.com cdn.jsdelivr.net;
-  frame-src giscus.app 
+  frame-src giscus.app
 `
 
 const securityHeaders = [
